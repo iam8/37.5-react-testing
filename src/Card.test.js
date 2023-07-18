00c946a5618f6f871c940447cfedd83aa175692c
alexm <alex-m@outlook.com>
2023-07-17 23:56:31 -0700

    Remove unused const variables; improve format of Card JSX to render

--- src/Card.test.js
+++ src/Card.test.js
@@ -7,13 +7,15 @@ import Card from "./Card.js";
 import TEST_IMAGES from "./_testCommon.js";
 
 const testImg1 = TEST_IMAGES[0];
-const testImg2 = TEST_IMAGES[1];
-const testImg3 = TEST_IMAGES[2];
 const totalNum = TEST_IMAGES.length;
 
 
 it("Renders without crashing", () => {
-    render(<Card caption={testImg1.caption} src={testImg1.src} currNum={0} totalNum={totalNum} />);
+    render(<Card
+        caption={testImg1.caption}
+        src={testImg1.src}
+        currNum={0}
+        totalNum={totalNum} />);
 });
 
 
